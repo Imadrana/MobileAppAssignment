ecbefa2d629668df1f9e15e6b4c1c68249c2060d
Fazan Khan <fazan.khan@edu.sait.ca>
2025-02-12 09:32:13 -0700

    fk

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,3 +1,4 @@
+/*FK*/
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import CityScreen from "./CityScreen";
@@ -5,16 +6,18 @@ import CityScreen from "./CityScreen";
 const Tab = createBottomTabNavigator();
 
 const WelcomeScreen = () => {
-    return (
-        <Tab.Navigator>
-            <Tab.Screen name="Calgary">
-                {() => <CityScreen city="Calgary" link="https://www.calgary.ca/home.html" />}
-            </Tab.Screen>
-            <Tab.Screen name="Edmonton">
-                {() => <CityScreen city="Edmonton" link="https://www.edmonton.ca/" />}
-            </Tab.Screen>
-        </Tab.Navigator>
-    );
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Calgary">
+        {() => (
+          <CityScreen city="Calgary" link="https://www.calgary.ca/home.html" />
+        )}
+      </Tab.Screen>
+      <Tab.Screen name="Edmonton">
+        {() => <CityScreen city="Edmonton" link="https://www.edmonton.ca/" />}
+      </Tab.Screen>
+    </Tab.Navigator>
+  );
 };
 
 export default WelcomeScreen;
